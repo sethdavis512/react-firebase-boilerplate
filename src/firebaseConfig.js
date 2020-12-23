dbbfa567548adea5421d49e73155a015fadb0350
Seth Davis <sethdavis@indeed.com>
2020-12-23 12:08:41 -0600

    Update firebase config data

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,7 +5,6 @@ const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
